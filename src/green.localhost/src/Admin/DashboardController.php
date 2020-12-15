9823eeb09e67af644e267e4fb83414bc09ac9cbf
EtienneMela <etienne.mela@edu.devinci.fr>
2020-12-15 01:25:18 +0100

    Added admin parts

--- src/green.localhost/src/Admin/DashboardController.php
+++ src/green.localhost/src/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Admin;
 
+use App\Entity\GetDataApi;
+use App\Entity\GetDataDataset;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -16,18 +19,24 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/dashboard-home.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Green Localhost');
+            ->setTitle('Espace d\'administration de l\'API')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
+        yield MenuItem::section('Application');
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Auhtorizations');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+        yield MenuItem::section('Api Managment');
+        yield MenuItem::linkToCrud('Api Externes', 'fa fa-tags', GetDataApi::class);
+        yield MenuItem::linkToCrud('Datasets Externes', 'fa fa-tags', GetDataDataset::class);
     }
 }
